1a3e9e84e3f908e5d7804aa7d12f065aa088d31d
Robin <robin.morand@gmail.com>
2024-07-27 20:26:02 +0200

    ajout fichier test clash of code

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Test {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String o = in.nextLine();
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        List<String> janken = Arrays.asList("Scissors", "Stone", "Hand");
+        if (janken.contains(o)) {
+            if (janken.indexOf(o) == janken.size() - 1){
+                System.out.println(janken.get(0));
+            } else {
+                System.out.println(janken.get(janken.indexOf(o) + 1));
+            }
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
